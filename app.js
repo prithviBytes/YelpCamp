451f5e9490be68da62cd33b8255f2f1f568c1148
Prithviraj Suvarna <prithvisuvarna56@gmail.com>
2020-07-12 05:01:08 +0000

    Improved sorting

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(require("express-session")({
 	resave : false,
 	saveUninitialized : false
 }));
-
-mongoose.connect('mongodb://localhost:27017/yelp_camp',
+// mongodb://localhost:27017/yelp_camp
+mongoose.connect('mongodb+srv://prithvisuvarna56:Lionelronaldo@1@cluster0.t4mwf.mongodb.net/<dbname>?retryWrites=true&w=majority',
 {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
